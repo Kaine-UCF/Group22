5511a24d4e6263a234e568d88be8decba3f79573
Jero <jero96@knights.ucf.edu>
2019-09-29 05:20:57 -0400

    fixed the footer link

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -4,7 +4,7 @@ export default () => {
   return (
     <footer className="bg-dark text-white mt-5 p-4 text-center">
       Copyright &copy; {new Date().getFullYear()}  -PeopleDex-
-      <div>Icons made by <a href="https://www.flaticon.com/<?=_('authors/')?>roundicons-freebies" title="Roundicons Freebies">Roundicons Freebies</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+      <div>Icons made by <a href="https://www.flaticon.com/packs/pokemon-go" title="Roundicons Freebies">Roundicons Freebies</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
       <div>Custom font from <a href="https://fontmeme.com/pokemon-font/">www.fontmeme.com</a></div>
       
     </footer>
